d819f53918eea53e6c301b0f69fc2f21358f96a8
CeciLlarena <104098086+CeciLlarena@users.noreply.github.com>
2022-05-17 20:48:36 -0600

    Add files via upload

---
+++ tic-tac-toe-bot-ceci/bot/utils.py
@@ -0,0 +1,282 @@
+"""
+[Module] Tic-tac-toe bot utilities.
+"""
+from random import randint
+import requests
+from urllib.parse import unquote
+
+
+API_URL = "http://127.0.0.1:8000"
+
+
+def is_registry_open() -> bool:
+    """
+    Checks if registry is available via API.
+    """
+    try:
+        url = "{}/registry".format(API_URL)
+        res = requests.get(url)
+
+        if res.text == "true":
+            return True
+        elif res.text == "false":
+            return False
+
+    except:
+        return False
+
+
+def register_user(name: str) -> str:
+    """
+    Registers user in API game.
+    """
+    url = "{}/register_player/{}".format(API_URL, name)
+    res = requests.post(url)
+    player_id = res.text[1]
+    return player_id
+
+
+def is_my_turn(player_id: str) -> bool: 
+    """
+    Checks if it is our turn via API.
+    """
+    url = "{}/turn/{}".format(API_URL, player_id)
+    res = requests.get(url)
+    
+    if res.text == "true":
+        return True
+    elif res.text == "false":
+        return False
+
+
+def read_board() -> list:
+    """
+    Gets game board via API.
+    """
+    url = "{}/board".format(API_URL)
+    res = requests.get(url)
+    board_str = res.text
+    board = [
+        [board_str[1], board_str[2], board_str[3]], 
+        [board_str[4], board_str[5], board_str[6]], 
+        [board_str[7], board_str[8], board_str[9]]
+    ]
+    return board
+
+
+def decide_move(board: list, player_id: str) -> list:
+    """
+    Decides next move to make.
+    """
+    enemigo = '0'
+    if player_id == '0':
+        enemigo == 'X'
+
+    ###Siempre elegir la posicion de en medio
+    if board [1][1] == "-":
+        return [1,1]
+    ###Si ya esta tomada, que elija la posicion de arriba a la izquierda
+    if board [1][1] == enemigo and board [0][0] == "-":
+        return [0,0]
+
+    
+### Si me toco de primero, significa que esatoy en la posicion del medio.
+### Calcular mi siguiente movimiento siemore yendo a izquiera arriba
+    if board [1][1] == player_id and board [0][0] == '-':
+        return [0,0]
+    
+    
+
+
+###Si soy 0, en el segundo turno defender, DEFENDER en el segundo turno. Bloquear cuando estan dos juntos, y cuando hay un espacio en medio
+    if board [0][0] == enemigo and board [0][1] == enemigo and board [0][2] == '-':
+        return [0,2]
+    if board [0][2] == enemigo and board [0][1] == enemigo and board [0][0] == '-':
+        return [0,0]
+    if board [0][0] == enemigo and board [0][2] == enemigo and board [0][1] == '-':
+        return [0,1]
+    
+    if board [1][2] == enemigo and board [1][1] == enemigo and board [1][0] == '-':
+        return [1,0]
+    if board [1][0] == enemigo and board [1][1] == enemigo and board [1][2] == '-':
+        return [1,2]
+    if board [1][0] == enemigo and board [1][2] == enemigo and board [1][1] == '-':
+        return [1,1]
+
+    if board [2][0] == enemigo and board [2][1] == enemigo and board [2][2] == '-':
+        return [2,2]
+    if board [2][2] == enemigo and board [2][1] == enemigo and board [2][0] == '-':
+        return [2,0]
+    if board [2][0] == enemigo and board [2][2] == enemigo and board [2][1] == '-':
+        return [2,1]
+    
+
+    if board [0][0] == enemigo and board [1][0] == enemigo and board [2][0] == '-':
+        return [2,0]
+    if board [2][0] == enemigo and board [1][0] == enemigo and board [0][0] == '-':
+        return [0,0]
+    if board [0][0] == enemigo and board [2][0] == enemigo and board [1][0] == '-':
+        return [1,0]
+
+    
+    if board [0][1] == enemigo and board [1][1] == enemigo and board [2][1] == '-':
+        return [2,1]
+    if board [2][1] == enemigo and board [1][1] == enemigo and board [0][1] == '-':
+        return [0,1]
+    if board [0][1] == enemigo and board [2][1] == enemigo and board [1][1] == '-':
+        return [1,1]
+
+
+    if board [0][2] == enemigo and board [1][2] == enemigo and board [2][2] == '-':
+        return [2,2]
+    if board [2][2] == enemigo and board [1][2] == enemigo and board [0][2] == '-':
+        return [0,2]
+    if board [0][2] == enemigo and board [2][2] == enemigo and board [1][2] == '-':
+        return [1,2]
+
+###para esquinas
+
+    if board [0][0] == enemigo and board [1][1] == enemigo and board [2][2] == '-':
+        return [2,2]
+    if board [2][2] == enemigo and board [1][1] == enemigo and board [0][0] == '-':
+        return [0,0]
+    if board [2][2] == enemigo and board [0][0] == enemigo and board [1][1] == '-':
+        return [1,1]
+    if board [0][2] == enemigo and board [1][1] == enemigo and board [2][0] == '-':
+        return [2,0]
+    if board [2][0] == enemigo and board [1][1] == enemigo and board [0][2] == '-':
+        return [0,2]
+    if board [0][2] == enemigo and board [2][0] == enemigo and board [1][1] == '-':
+        return [1,1]
+
+
+
+
+
+
+###ESTRATEGIAS PARA ATACAR (GANAR)
+
+###de izquiera a derecha y arriba para abajo
+    if board [0][0] == player_id and board [0][1] == player_id and board [0][2] == '-':
+        return [0,2]
+    if board [1][0] == player_id and board [1][1] == player_id and board [1][2] == '-':
+        return [1,2]
+    if board [2][0] == player_id and board [2][1] == player_id and board [2][2] == '-':
+        return [2,2]
+    if board [0][0] == player_id and board [1][0] == player_id and board [2][0] == '-':
+        return [2,0]
+    if board [0][1] == player_id and board [1][1] == player_id and board [2][1] == '-':
+        return [2,1]
+    if board [0][2] == player_id and board [1][2] == player_id and board [2][2] == '-':
+        return [2,2]
+###de derecha a izquierda y de abajo para arriba
+    if board [2][0] == player_id and board [1][0] == player_id and board [0][0] == '-':
+        return [0,0]
+    if board [2][1] == player_id and board [1][1] == player_id and board [0][1] == '-':
+        return [0,1]
+    if board [2][2] == player_id and board [1][2] == player_id and board [0][2] == '-':
+        return [0,2]
+    if board [0][2] == player_id and board [0][1] == player_id and board [0][0] == '-':
+        return [0,0]
+    if board [1][2] == player_id and board [1][1] == player_id and board [1][0] == '-':
+        return [1,0]
+    if board [2][2] == player_id and board [2][1] == player_id and board [2][0] == '-':
+        return [2,0]
+    
+###Lineas al medio
+    if board [0][0] == player_id and board [1][1] == player_id and board [2][2] == '-':
+        return [2,2]
+    if board [2][2] == player_id and board [1][1] == player_id and board [0][0] == '-':
+        return [0,0]
+    if board [0][2] == player_id and board [1][1] == player_id and board [2][0] == '-':
+        return [2,0]
+    if board [2][0] == player_id and board [1][1] == player_id and board [0][2] == '-':
+        return [0,2]
+
+### Si empiezo en el medio 
+    if board [1][1] == player_id and board [0][0] == player_id and board [2][2] == '-':
+        return [2,2]
+    if board [1][1] == player_id and board [2][2] == player_id and board [0][0] == '-':
+        return [0,0]
+    if board [1][1] == player_id and board [1][0] == player_id and board [1][2] == '-':
+        return [1,2]
+    if board [1][1] == player_id and board [2][2] == player_id and board [0][0] == '-':
+        return [0,0]
+    if board [1][1] == player_id and board [2][0] == player_id and board [0][2] == '-':
+        return [0,2]
+    if board [1][1] == player_id and board [0][2] == player_id and board [2][0] == '-':
+        return [2,0]
+    if board [1][1] == player_id and board [0][1] == player_id and board [2][1] == '-':
+        return [2,1]
+    if board [1][1] == player_id and board [2][1] == player_id and board [0][1] == '-':
+        return [0,1]
+
+###Si empiezo en las esquinas y tengo un hoyo en medio
+    if board [0][0] == player_id and board [0][2] == player_id and board [0][1] == '-':
+        return [0,1]
+    if board [2][0] == player_id and board [2][2] == player_id and board [2][1] == '-':
+        return [2,1]
+    if board [0][0] == player_id and board [2][2] == player_id and board [1][1] == '-':
+        return [1,1]
+    if board [1][0] == player_id and board [1][2] == player_id and board [1][1] == '-':
+        return [1,1]
+    if board [2][0] == player_id and board [0][2] == player_id and board [1][1] == '-':
+        return [1,1]
+    if board [0][2] == player_id and board [2][2] == player_id and board [1][2] == '-':
+        return [1,2]
+    if board [0][1] == player_id and board [2][1] == player_id and board [1][1] == '-':
+        return [1,1]
+    if board [0][0] == player_id and board [2][0] == player_id and board [1][0] == '-':
+        return [1,0]
+    
+    row = randint(0,2)
+    column = randint(0,2)
+    return [row,column]
+
+
+
+def validate_move(board: list, move: list) -> bool:
+    """
+    Checks if the desired next move hits an empty position.
+    """
+    row, col = move[0], move[1]
+
+    if board[row][col] == "-":
+        return True
+
+    return False
+
+
+def send_move(player_id: str, move: list) -> None:
+    """
+    Sends move to API.
+    """
+    row, col = move[0], move[1]
+    url = "{}/move/{}/{}/{}".format(API_URL, player_id, row, col)
+    res = requests.post(url)
+    return None
+
+
+def does_game_continue() -> bool:
+    """
+    Checks if the current match continues via API.
+    """
+    url = "{}/continue".format(API_URL)
+    res = requests.get(url)
+
+    if res.text == "true":
+        return True
+    elif res.text == "false":
+        return False
+
+
+def print_board(board: list) -> None:
+    '''
+    Prints the baord in console to watch the game.
+    '''
+    print("\nCurrent board: \n")
+    print(board[0][0], "|", board[0][1], "|", board[0][2])
+    print("----------")
+    print(board[1][0], "|", board[1][1], "|", board[1][2])
+    print("----------")
+    print(board[2][0], "|", board[2][1], "|", board[2][2], "\n")
